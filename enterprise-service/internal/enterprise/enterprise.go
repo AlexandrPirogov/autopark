1a53597b868f214bf7ff03b362c7be8f0c7ab959
edual <devalexandrpirogov@gmail.com>
2023-08-24 13:31:07 -0400

    added Compare method for enterprise struct

--- enterprise-service/internal/enterprise/enterprise.go
+++ enterprise-service/internal/enterprise/enterprise.go
@@ -1,6 +1,12 @@
 // enterprise package works with enterprise entity
 package enterprise
 
+import "strings"
+
 type Enterprise struct {
 	Title string `json:"title"`
+}
+
+func (e Enterprise) Compare(with Enterprise) int {
+	return strings.Compare(e.Title, with.Title)
 }
