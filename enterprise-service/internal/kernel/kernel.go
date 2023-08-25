5668542c4c7352d2073c5bc798aabb2126d6946c
edual <devalexandrpirogov@gmail.com>
2023-08-25 06:29:46 -0400

    moved interface EnterpriseStorer to db package

--- enterprise-service/internal/kernel/kernel.go
+++ enterprise-service/internal/kernel/kernel.go
@@ -2,24 +2,17 @@
 package kernel
 
 import (
+	"enterprise-service/internal/db"
 	"enterprise-service/internal/enterprise"
 	"enterprise-service/internal/std"
 )
-
-// EnterpriseStorer stands for working with storer to save enterprise entyties
-type EntepriseStorer interface {
-	Delete(e enterprise.Enterprise) error
-	Read(e enterprise.Enterprise) (std.Linked[enterprise.Enterprise], error)
-	Store(e enterprise.Enterprise) error
-	Update(e enterprise.Enterprise) (std.Linked[enterprise.Enterprise], error)
-}
 
 // Delete removes all enterprise entities from given EnterpriseStore which mathces given pattern
 //
 // Pre-cond: given pattern and Storer to remove enterprise entityies from
 //
-// Post-cond: all enterprises which matches given pattern was removed from given EnterpriseStorer
-func Delete(pattern enterprise.Enterprise, s EntepriseStorer) error {
+// Post-cond: all enterprises which matches given pattern was removed from given EntepriseStorer
+func Delete(pattern enterprise.Enterprise, s db.EntepriseStorer) error {
 	return s.Delete(pattern)
 }
 
@@ -27,8 +20,8 @@ func Delete(pattern enterprise.Enterprise, s EntepriseStorer) error {
 //
 // Pre-cond: given pattern and Storer to returns enterprise entities from
 //
-// Post-cond: all enterprises which matches given pattern was returned from given EnterpriseStorer
-func Read(pattern enterprise.Enterprise, s EntepriseStorer) (std.Linked[enterprise.Enterprise], error) {
+// Post-cond: all enterprises which matches given pattern was returned from given EntepriseStorer
+func Read(pattern enterprise.Enterprise, s db.EntepriseStorer) (std.Linked[enterprise.Enterprise], error) {
 	return s.Read(pattern)
 }
 
@@ -37,15 +30,15 @@ func Read(pattern enterprise.Enterprise, s EntepriseStorer) (std.Linked[enterpri
 // Pre-cond: given new unique enterprise entity to write and EnterpriseStore to write to
 //
 // Post-cond: given enterprise entity was written to EnterpriseStore
-func Store(e enterprise.Enterprise, s EntepriseStorer) error {
+func Store(e enterprise.Enterprise, s db.EntepriseStorer) error {
 	return s.Store(e)
 }
 
 // Update updates all enterprise entities from given EnterpriseStore which mathces given pattern
 //
-// Pre-cond: given pattern and EnterpriseStorer to update enterprise entities
+// Pre-cond: given pattern and EntepriseStorer to update enterprise entities
 //
-// Post-cond: all enterprises which matches given pattern was updated within given EnterpriseStorer
-func Update(pattern enterprise.Enterprise, s EntepriseStorer) (std.Linked[enterprise.Enterprise], error) {
+// Post-cond: all enterprises which matches given pattern was updated within given EntepriseStorer
+func Update(pattern enterprise.Enterprise, s db.EntepriseStorer) (std.Linked[enterprise.Enterprise], error) {
 	return s.Update(pattern)
 }
