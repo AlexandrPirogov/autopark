64a3a9b4fc13f65affe58040c3f81d32051b8328
edual <devalexandrpirogov@gmail.com>
2023-08-27 04:14:27 -0400

    added auth service

---
+++ auth-service/internal/storage/db/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"auth-service/internal/storage/db/postgres"
+	"auth-service/internal/storage/db/redis"
+)
+
+type CredentionalsStorer interface {
+	LookForAdmin(login, pwd string) bool
+}
+
+type JWTTokenStorer interface {
+	SetRefreshToken(val string) error
+}
+
+func GetCurrentCredsStorerInstance() CredentionalsStorer {
+	return postgres.GetInstance()
+}
+
+func GetCurrentJWTStorerInstance() JWTTokenStorer {
+	return redis.GetInstance()
+}
