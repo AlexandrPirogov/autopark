10589943bcc025665839a2a42b474ce836b4cf4e
edual <devalexandrpirogov@gmail.com>
2023-08-28 02:39:39 -0400

    added commentaries

--- auth-service/internal/storage/db/postgres/query.go
+++ auth-service/internal/storage/db/postgres/query.go
@@ -1,7 +1,10 @@
 package postgres
 
+// QueryLookForAdmin reads id of admin by given login and pwd
 const QueryLookForAdmin = `select id from admins where login = $1 and pwd = $2`
 
+// QueryLookForManager reads id of manager by given login and pwd
 const QueryLookForManager = `select id from managers where login = $1 and pwd = $2`
 
+// QueryInsertNewManager inserts new manager with given login and pwd.
 const QueryInsertNewManager = `insert into managers values(default, $1, $2)`
