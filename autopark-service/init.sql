bcd52c807740ac5f3d7c03fbc4d09b22f008d0f2
edual <devalexandrpirogov@gmail.com>
2023-08-28 07:08:06 -0400

    added init data into init.sql

--- autopark-service/init.sql
+++ autopark-service/init.sql
@@ -23,3 +23,18 @@ CREATE TABLE CARS_FOR_BOOKING (
     WATCHING INT,
     CONSTRAINT FK_CAR_FOR_BOOKING FOREIGN KEY (CAR_ID) REFERENCES CARS(ID) 
 );
+
+INSERT INTO BRANDS values(default, 'bmw');
+INSERT INTO BRANDS values(default, 'mets');
+INSERT INTO BRANDS values(default, 'gaz');
+
+INSERT INTO CARS values(default, 1, 'bmw1', 'sport');
+INSERT INTO CARS values(default, 2, 'mers1', 'sport');
+INSERT INTO CARS values(default, 3, 'gaz1', 'sport');
+INSERT INTO CARS values(default, 1, 'bmw2', 'sport');
+INSERT INTO CARS values(default, 2, 'mers2', 'sport');
+INSERT INTO CARS values(default, 3, 'gaz3', 'sport');
+
+INSERT INTO CARS_FOR_BOOKING values(default, 1, 'set', 0);
+INSERT INTO CARS_FOR_BOOKING values(default, 2, 'unset', 0);
+INSERT INTO CARS_FOR_BOOKING values(default, 3, 'set', 0);
