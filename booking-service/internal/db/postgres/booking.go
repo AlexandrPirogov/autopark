182c00c5fa078d87243d81365b38f3b1a9bb6ef3
edual <devalexandrpirogov@gmail.com>
2023-08-30 09:47:15 -0400

    added postgres functions to work with bookings

--- booking-service/internal/db/postgres/booking.go
+++ booking-service/internal/db/postgres/booking.go
@@ -8,31 +8,57 @@ import (
 	"time"
 )
 
+// QueryBookingApproved set status of booking to approved
 const QueryBookingApproved = `update bookings set status = 'approved', finished_at = now() where id = $1`
 
+// QueryBookingCanceled set status of booking to canceled
 const QueryBookingCanceled = `update bookings set status = 'canceled', finished_at = now() where id = $1`
 
+// QueryBookingCarChoosed set status of booking to car_choosed
 const QueryBookingCarChoosed = `update bookings set status = 'car_choosed', car_id = $1 where id = $2`
 
+// QueryBookingCreate inserts new record for booking
 const QueryBookingCreate = `insert into bookings values(default, $1, -1, $2, $3, $4) returning id`
 
+// QueryBookingFinished set status of booking to finished
+const QueryBookingFinished = `update bookings set status = 'finished', finished_at = now() where id = $1`
+
+// Approved executes QueryBookingApproved
+//
+// Pre-cond: given booking with set ID to approve
+//
+// Post-cond: query was executed. If successfull, returns nil. Otherwise returns error
 func (pg *pgconn) Approved(b entity.Booking) error {
 	_, err := pg.conn.Exec(context.Background(), QueryBookingApproved, b.ID)
 	return err
 }
 
+// Canceled executes QueryBookingCanceled
+//
+// Pre-cond: given booking with set ID to cancel
+//
+// Post-cond: query was executed. If successfull, returns nil. Otherwise returns error
 func (pg *pgconn) Canceled(b entity.Booking) error {
 	_, err := pg.conn.Exec(context.Background(), QueryBookingCanceled, b.ID)
 	return err
 }
 
+// CarChoosed executes QueryBookingCarChoosed
+//
+// Pre-cond: given booking with set ID to ChooseCAr and CarID
+//
+// Post-cond: query was executed. If successfull, returns nil. Otherwise returns error
 func (pg *pgconn) CarChoosed(b entity.Booking) error {
-	log.Println("choosing car booking writing into db ", b)
 	_, err := pg.conn.Exec(context.Background(), QueryBookingCarChoosed, b.CarID, b.ID)
-	log.Println("choosing car booking writing err ", err)
 	return err
 }
 
+// Created executes QueryBookingCreate
+//
+// Pre-cond: given booking with set userID
+//
+// Post-cond: query was executed. If successfull, returns id of newly created record.
+// Otherwise returns -1 and error
 func (pg *pgconn) Created(b entity.Booking) (int, error) {
 	var id int
 	err := pg.conn.QueryRow(context.Background(), QueryBookingCreate, b.UserID, bookingfsm.StateCreated, time.Now(), time.Now()).Scan(&id)
@@ -40,4 +66,15 @@ func (pg *pgconn) Created(b entity.Booking) (int, error) {
 		log.Println(err)
 	}
 	return id, err
+}
+
+// Finish executes QueryBookingFinish
+//
+// Pre-cond: given booking with set userID and set ID
+//
+// Post-cond: query was executed. If successfull returns nil.
+// Otherwise returns error
+func (pg *pgconn) Finish(b entity.Booking) error {
+	_, err := pg.conn.Exec(context.Background(), QueryBookingFinished, b.ID)
+	return err
 }
