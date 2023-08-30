5ce75a26b551480af7761100c0de959b496c71dd
edual <devalexandrpirogov@gmail.com>
2023-08-30 10:34:37 -0400

    added commentaries

--- client-service/internal/kernel/kernel.go
+++ client-service/internal/kernel/kernel.go
@@ -3,6 +3,7 @@ package kernel
 
 import (
 	"client-service/internal/client"
+	"client-service/internal/entity"
 	"client-service/internal/entity/autopark"
 )
 
@@ -26,4 +27,49 @@ func ReadBrands(pattern autopark.Brand, c client.ClientAutopark) ([]autopark.Bra
 // Otherwise returnes nil and error
 func ReadCars(pattern autopark.Car, c client.ClientAutopark) ([]autopark.Car, error) {
 	return c.ReadCars(pattern)
+}
+
+// CreateBooknig making a request to create new book in booking-service
+//
+// Pre-cond: given entity.ClientCreds to create new booking for and implemtation of client.ClientBooking
+//
+// Post-cond: client.ClientBooking executes request and returns result of request
+func CreateBooking(e entity.ClientCreds, c client.ClientBooking) (entity.Booking, error) {
+	return c.BookingCreate(e)
+}
+
+// ChooseCarBooking making a request to choose car for given booking in booking-service
+//
+// Pre-cond: given entity.Booking to choose car for and implemtation of client.ClientBooking
+//
+// Post-cond: client.ClientBooking executes request and returns result of request
+func ChooseCarBooking(b entity.Booking, c client.ClientBooking) error {
+	return c.BookingChoose(b)
+}
+
+// ApproveBooking making a request to approve given book in booking-service
+//
+// Pre-cond: given entity.Booking to approve and implemtation of client.ClientBooking
+//
+// Post-cond: client.ClientBooking executes request and returns result of request
+func ApproveBooking(b entity.Booking, c client.ClientBooking) (entity.Booking, error) {
+	return c.BookingApprove(b)
+}
+
+// CancelBooking making a request to cancel given book in booking-service
+//
+// Pre-cond: given entity.Booking to cancel and implemtation of client.ClientBooking
+//
+// Post-cond: client.ClientBooking executes request and returns result of request
+func CancelBooking(b entity.Booking, c client.ClientBooking) error {
+	return c.BookingCancel(b)
+}
+
+// FinishBooking making a request to finish given book in booking-service
+//
+// Pre-cond: given entity.Booking to finish implemtation of client.ClientBooking
+//
+// Post-cond: client.ClientBooking executes request and returns result of request
+func FinishBooking(b entity.Booking, c client.ClientBooking) error {
+	return c.BookingFinish(b)
 }
