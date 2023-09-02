d1c0d51dca7de2e8ec121e0aa96d664694c2f382
edual <devalexandrpirogov@gmail.com>
2023-09-02 11:18:19 -0400

    added dot env and zerolog packages

---
+++ auth-service/internal/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+// configurable variable
+var (
+	redis_pwd     string
+	postgres_user string
+	postgres_pwd  string
+	postgres_db   string
+
+	log_level string
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal().Msgf("Error loading .env file")
+	}
+
+	redis_pwd = os.Getenv("REDIS_PASSWORD")
+
+	postgres_user = os.Getenv("POSTGRES_USER")
+	postgres_pwd = os.Getenv("POSTGRES_PASSWORD")
+	postgres_db = os.Getenv("POSTGRES_DB")
+
+	log_level = os.Getenv("LOG_LEVEL")
+
+	levels := LogLevelMap()
+	if _, ok := levels[log_level]; !ok {
+		log.Fatal().Msgf("there are only debug|info|warn log levels")
+	}
+
+	zerolog.SetGlobalLevel(levels[log_level])
+	log.Info().Msgf("Log level is %s", levels[log_level])
+}
+
+// returns redis password
+func RedisPwd() string {
+	return redis_pwd
+}
+
+// returns postgres url to connect to
+func PostgresURL() string {
+	return "postgresql://" + postgres_user + ":" + postgres_pwd + "@postgres-auth:5432/" + postgres_db
+}
+
+func LogLevelMap() map[string]zerolog.Level {
+	return map[string]zerolog.Level{
+		"debug":   zerolog.DebugLevel,
+		"info":    zerolog.InfoLevel,
+		"warning": zerolog.WarnLevel,
+	}
+}
